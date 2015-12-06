05129a310f539c4b2a253c79ec9cc5b51176248b
Daniel Seo <applypattern@naver.com>
2015-12-07 00:05:46 +0900

    3.28~3.29 문제 풀이

--- fpinscala/src/main/scala/datastructure/Tree.scala
+++ fpinscala/src/main/scala/datastructure/Tree.scala
@@ -22,7 +22,27 @@ object Tree {
   //3.27  
   def depth[A](tree: Tree[A]): Int = tree match {
     case Leaf(_)      => 0
-    case Branch(l, r) => 1 + depth(l).max(depth(r))
+    case Branch(l, r) => 1 + (depth(l) max depth(r))
   }
+
+  // 3.28
+  def map[A, B](tree: Tree[A])(f: A => B): Tree[B] = tree match {
+    case Branch(l, r) => Branch(map(l)(f), map(r)(f))
+    case Leaf(v)      => Leaf(f(v))
+  }
+
+  // 3.29 fold
+  def fold[A, B](tree: Tree[A])(l: A => B)(b: (B, B) => B): B = tree match {
+    case Branch(lb, rb) => b(fold(lb)(l)(b), fold(rb)(l)(b))
+    case Leaf(v)        => l(v)
+  }
+
+  def sizeViaFold[A](tree: Tree[A]): Int = fold(tree)(a => 1)(_ + _ + 1)
+
+  def maximumViaFold(tree: Tree[Int]): Int = fold(tree)(a => a)(_ max _)
+
+  def depthViaFold[A](tree: Tree[A]): Int = fold(tree)(a => 0)((c, d) => 1 + (c max d))
+
+  def mapViaFold[A, B](tree: Tree[A])(f: A => B): Tree[B] = fold(tree)(a => Leaf(f(a)): Tree[B])(Branch(_, _))
 
 }
--- fpinscala/src/test/scala/datastructure/TreeTest.scala
+++ fpinscala/src/test/scala/datastructure/TreeTest.scala
@@ -3,18 +3,49 @@ package datastructure
 import org.scalatest.FunSuite
 
 class TreeTest extends FunSuite {
-  test("should return size of tree ") {
-    assert(7 == Tree.size(Branch(Branch(Leaf(1), Leaf(2)), Branch(Leaf(4), Leaf(5)))))
-    assert(11 == Tree.size(Branch(Branch(Leaf(6), Leaf(10)), Branch(Branch(Leaf(1), Leaf(2)), Branch(Leaf(4), Leaf(5))))))
+  test("should return size of tree") {
+    val numberTree = Branch(Branch(Leaf(1), Leaf(2)), Branch(Leaf(4), Leaf(5)))
+    val extendedTree = Branch(Branch(Leaf(6), Leaf(10)), Branch(Branch(Leaf(1), Leaf(2)), Branch(Leaf(4), Leaf(5))))
+    assert(7 == Tree.size(numberTree))
+    assert(11 == Tree.size(extendedTree))
   }
 
-  test("should return the number of maximum  of tree ") {
+  test("should return the number of maximum of tree") {
     assert(5 == Tree.maximum(Branch(Branch(Leaf(1), Leaf(2)), Branch(Leaf(4), Leaf(5)))))
     assert(10 == Tree.maximum(Branch(Branch(Leaf(6), Leaf(10)), Branch(Branch(Leaf(1), Leaf(2)), Branch(Leaf(4), Leaf(5))))))
   }
 
-  test("should return the maximum depth of tree ") {
+  test("should return the maximum depth of tree") {
     assert(2 == Tree.depth(Branch(Branch(Leaf(1), Leaf(2)), Branch(Leaf(4), Leaf(5)))))
     assert(3 == Tree.depth(Branch(Branch(Leaf(6), Leaf(10)), Branch(Branch(Leaf(1), Leaf(2)), Branch(Leaf(4), Leaf(5))))))
+  }
+
+  test("should return results using map method") {
+    val tree = Branch(Branch(Leaf(1), Leaf(2)), Branch(Leaf(3), Leaf(4)))
+    val resultTree = Branch(Branch(Leaf(3), Leaf(4)), Branch(Leaf(5), Leaf(6)))
+    assert(resultTree == Tree.map(tree)(v => v + 2))
+  }
+
+  test("should return size of tree Using sizeViaFold") {
+    val numberTree = Branch(Branch(Leaf(1), Leaf(2)), Branch(Leaf(4), Leaf(5)))
+    val extendedTree = Branch(Branch(Leaf(6), Leaf(10)), Branch(Branch(Leaf(1), Leaf(2)), Branch(Leaf(4), Leaf(5))))
+    assert(7 == Tree.sizeViaFold(numberTree))
+    assert(11 == Tree.sizeViaFold(extendedTree))
+  }
+
+  test("should return the number of maximum  of tree using maximumViaFold") {
+    assert(5 == Tree.maximumViaFold(Branch(Branch(Leaf(1), Leaf(2)), Branch(Leaf(4), Leaf(5)))))
+    assert(10 == Tree.maximumViaFold(Branch(Branch(Leaf(6), Leaf(10)), Branch(Branch(Leaf(1), Leaf(2)), Branch(Leaf(4), Leaf(5))))))
+  }
+
+  test("should return the maximum depth of tree using depthViaFold") {
+    assert(2 == Tree.depthViaFold(Branch(Branch(Leaf(1), Leaf(2)), Branch(Leaf(4), Leaf(5)))))
+    assert(3 == Tree.depthViaFold(Branch(Branch(Leaf(6), Leaf(10)), Branch(Branch(Leaf(1), Leaf(2)), Branch(Leaf(4), Leaf(5))))))
+  }
+
+  test("should return results using mapViaFold method") {
+    val tree = Branch(Branch(Leaf(1), Leaf(2)), Branch(Leaf(3), Leaf(4)))
+    val resultTree = Branch(Branch(Leaf(3), Leaf(4)), Branch(Leaf(5), Leaf(6)))
+    assert(resultTree == Tree.mapViaFold(tree)(v => v + 2))
   }
 }
