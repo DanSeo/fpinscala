23d179d3d7d10c1ba83075916ac39aaa020acce0
Daniel Seo <applypattern@naver.com>
2015-12-09 22:52:56 +0900

    4.2 문제 풀이

--- fpinscala/src/main/scala/errorhandling/Option.scala
+++ fpinscala/src/main/scala/errorhandling/Option.scala
@@ -1,19 +1,25 @@
 package errorhandling
 
-trait Option[+A] {
+sealed trait Option[+A] extends Any {
   def map[B](f: A => B): Option[B] = this match {
     case Some(a) => Some[B](f(a))
-    case _ => None
+    case _       => None
   }
   def flatMap[B](f: A => Option[B]): Option[B] = map(f) getOrElse None
-  
+
   def getOrElse[B >: A](default: => B): B = this match {
+    case None       => default
     case Some(a) => a
-    case _ => default
   }
   def orElse[B >: A](ob: => Option[B]): Option[B] = this map (Some(_)) getOrElse ob
-  
-  def filter(f: A => Boolean): Option[A] = flatMap(a => if(f(a)) Some(a) else None) 
+
+  def filter(f: A => Boolean): Option[A] = flatMap(a => if (f(a)) Some(a) else None)
 }
 case class Some[+A](get: A) extends Option[A]
 case object None extends Option[Nothing]
+
+object Math {
+  def mean(xs: Seq[Double]): Option[Double] = if (xs.isEmpty) None else Some(xs.sum / xs.length)
+  // 4.2 
+  def variance(xs: Seq[Double]): Option[Double] = mean(xs).flatMap { m => mean(xs.map(x => math.pow(x - m, 2))) }
+}
