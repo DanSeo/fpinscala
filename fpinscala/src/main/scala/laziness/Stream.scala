5ac756ad0c1cb4210fba884678c72f2c1f7eec84
Daniel Seo <applypattern@naver.com>
2015-12-16 20:23:20 +0900

    5.2~5.3 연습문제 풀이

--- fpinscala/src/main/scala/laziness/Stream.scala
+++ fpinscala/src/main/scala/laziness/Stream.scala
@@ -29,21 +29,28 @@ sealed trait Stream[+A] {
     go(this, List()).reverse
   }
   // TOOD ListBuffer 를 사용하는 방법을 쓰자
-  
+
   // 5.2
-  //def take(n: Int): Stream[A] = 
-  
+  def take(n: Int): Stream[A] = this match {
+    case Cons(h, t) if n > 1  => Stream.cons(h(), t().take(n - 1))
+    case Cons(h, _) if n == 0 => Stream.cons(h(), Empty)
+    case _                    => Empty
+  }
+
   def drop(n: Int): Stream[A] = {
     @tailrec
-    def go(s: Stream[A] ,n :Int) : Stream[A] = s match {
+    def go(s: Stream[A], n: Int): Stream[A] = s match {
       case Cons(h, t) if n > 0 => go(t(), n - 1)
-      case _ => s
+      case _                   => s
     }
     go(this, n)
   }
-  
+
   // 5.3 
-  //def takeWhile(p: A => Boolean): Stream[A]
+  def takeWhile(p: A => Boolean): Stream[A] = this match {
+    case Cons(h, t) if p(h()) => Stream.cons(h(), t().takeWhile(p))
+    case _                    => Empty
+  }
 }
 case object Empty extends Stream[Nothing]
 case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
